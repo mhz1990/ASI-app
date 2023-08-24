a2a1a14bc673ee774c888c02ce748ea3e1180fb0
ShuwenZhao <106790068+ShuwenZhao@users.noreply.github.com>
2023-08-24 14:18:56 -0700

    Revert "Add a new page as a component called EditProfile"

--- ArtSphere/app/(tabs)/home/profile/index.js
+++ ArtSphere/app/(tabs)/home/profile/index.js
@@ -1,44 +1,20 @@
 import { Stack, useRouter } from "expo-router";
-import { useState } from "react";
-import {
-  Button,
-  View,
-  Modal,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Image,
-} from "react-native";
+import { Button,View,StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import ProfileInfo from "../../../../components/ProfileInfo";
 import ProfileList from "../../../../components/ProfileList";
-import EditProfile from "../../../../components/EditProfile";
+export default ()=>{
+    const router = useRouter();
+    return (
+        <View>
+            <Stack.Screen options={{
+                title:"Profile",
+                headerStyle:{ backgroundColor: "#106256"},
+                headerTintColor: '#fff',
+                }}
+            />
+            <ProfileInfo />
+            <ProfileList />
+        </View>
+    )
+}
 
-export default () => {
-  const [isEditing, setIsEditing] = useState(false);
-
-  const onPress = () => {
-    setIsEditing(true);
-  };
-
-  const closeModal = () => {
-    setIsEditing(false);
-  };
-
-  const router = useRouter();
-  return (
-    <View>
-      <Stack.Screen
-        options={{
-          title: "Profile",
-          headerStyle: { backgroundColor: "#106256" },
-          headerTintColor: "#fff",
-        }}
-      />
-      <ProfileInfo onPress={onPress} />
-      <ProfileList />
-      <Modal animationType="slide" transparent={false} visible={isEditing}>
-        <EditProfile closeModal={closeModal} />
-      </Modal>
-    </View>
-  );
-};
